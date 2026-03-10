b3c44f4f10612c76988c4270ef1e4c36c84751d4
greeenlaser <sanderveski@gmail.com>
2026-03-10 22:23:21 +0200

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -33,7 +33,7 @@ static void AddExternalCommands()
 		};
 	auto version = [](const vector<string>& params)
 		{
-			Log::Print("prerelease 9");
+			Log::Print("prerelease 10");
 		};
 
 	CommandManager::AddCommand(
